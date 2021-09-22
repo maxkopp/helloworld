ccd551aa55cae29de1f93ebb25a98fc77875c2a4
maximiliankopp <maximiliankopp@10.1.247.46>
2021-09-22 09:17:34 +0200

    first

---
+++ src/Glücksspiel/Glücksspiel.java
@@ -0,0 +1,24 @@
+package Glücksspiel;
+
+
+public class Glücksspiel{
+	static byte alter=19;
+	static byte geschlecht=2;
+	static String name ="maximilian Kopp";
+
+	public static void main(String[] args) {
+		if (geschlecht == 2 || alter < 17){
+			System.out.println("Hallo" + name + "!");
+			
+		}
+		
+	else if (geschlecht == 1 ) {
+				System.out.println("Guten Tag Frau" + name + "!");
+	}
+			 
+		}}
+		
+		
+
+
+	
